be55670a0274a160fee80ae3b7f0a7566452769c
Martin Swiderski <martinswiderski@googlemail.com>
2018-05-03 12:16:55 +0100

    create-db

---
+++ lesson-01/step-01/create-db-addresses.sql
@@ -0,0 +1,4 @@
+/*** SQL DDL creates database ***/
+CREATE DATABASE addresses
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
---
+++ lesson-01/step-01/create-db-postcodes.sql
@@ -0,0 +1,4 @@
+/*** SQL DDL creates database ***/
+CREATE DATABASE postcodes
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
